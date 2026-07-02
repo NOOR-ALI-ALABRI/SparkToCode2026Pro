becab91a63619019f4f5b4c132afac3130c85298
Noor <noor-al-abri@hotmail.com>
2026-07-02 16:10:06 +0400

    Task 2 - Rectangle Calculator

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        //Task 1 - Personal Info Card
+
+        string name = "sara";
+        int age = 21;
+        double height = 1.65;
+        bool student = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Student: " + student);
+        
+        
+       //Task 2 - Rectangle Calculator
+
+        Console.Write("Please Enter Your length: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("Please Enter Your width: ");
+        double width = double.Parse(Console.ReadLine());
+
+        double Area = length * width;
+        double Perimeter = 2 * (length + width);
+
+        Console.WriteLine("the Area:" + Area);
+        Console.WriteLine("the Perimeter:" + Perimeter);
+    }
+}
