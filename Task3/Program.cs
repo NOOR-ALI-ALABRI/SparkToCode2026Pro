a45cca3c259d45405b1a28ea717214a6aea467eb
Noor <noor-al-abri@hotmail.com>
2026-07-05 15:35:06 +0400

    Task 1 - Absolute Difference

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     //Task 1 - Absolute Difference
+     Console.WriteLine("Enter Two Number");
+     int num1 = int.Parse(Console.ReadLine());
+     int num2= int.Parse(Console.ReadLine());
+     int result = num1 - num2;
+      
+      Console.WriteLine(Math.Abs(result));
+
+      
+
+
+
+    }
+}
