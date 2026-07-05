120bb2d41db9a7489ce5babed641e5562bb5bd05
Noor <noor-al-abri@hotmail.com>
2026-07-05 17:01:28 +0400

    Task 5 - Grade Rounding System

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -36,7 +36,7 @@ class Program
       Console.WriteLine( "name in upper case:"+ name.ToUpper());
       Console.WriteLine("name in lower case :"+ name.ToLower());
       Console.WriteLine("the Length of your na"+name.Length );
-      */
+   
         /////////////////////////////////////////
         
         //Task 4 - Subscription End Date
@@ -53,9 +53,33 @@ class Program
 
         Console.WriteLine(endDate.ToString("yyyy-MM-dd"));
 
+        */
+        /////////////////////////////////////////
+        
+       //Task 5 - Grade Rounding System
+       
+     Console.WriteLine("Enter your raw exam score (e.g., 74.6)");
+     double score = double.Parse(Console.ReadLine());
 
-
-
-
+     double roundScore= Math.Round(score, 0);
+     Console.WriteLine($"Rounded Score: {roundScore}");
+     
+     if (roundScore >= 60)
+     {
+         Console.WriteLine("Result: Pass");
+     }
+     else
+     {
+         Console.WriteLine("Result: Fail");
+     }
+     
+     
+   
+     
+     
+     
+     
+     
+     
     }
 }
