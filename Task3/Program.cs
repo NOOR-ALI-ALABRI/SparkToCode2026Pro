c6718bb3204ba2754def9d399f743626eef4c33e
Noor <noor-al-abri@hotmail.com>
2026-07-05 15:41:41 +0400

    Task 2 - Power & Root Explorer

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
      //Task 1 - Absolute Difference
      Console.WriteLine("Enter Two Number");
      int num1 = int.Parse(Console.ReadLine());
@@ -11,10 +12,22 @@ class Program
      int result = num1 - num2;
       
       Console.WriteLine(Math.Abs(result));
-
+     */
       
+     //Task 2 - Power & Root Explorer
+     Console.WriteLine("Enter a number ");
+     int number = int.Parse(Console.ReadLine());
 
-
-
+     double power = Math.Pow(number, 2);
+     double sqrt = Math.Sqrt(number);
+     Console.WriteLine("the power of number is" + power);
+     Console.WriteLine("the sqrt of number is" + sqrt);
+     
+     
+     
+     
+     
+     
+     
     }
 }
