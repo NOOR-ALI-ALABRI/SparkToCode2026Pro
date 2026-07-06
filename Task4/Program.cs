c55a9339ab1c164032bbc6638ff11a00eefcf031
Noor <noor-al-abri@hotmail.com>
2026-07-06 21:47:56 +0400

    Task 2 - Square Number Function

---
+++ Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task4;
+
+class Program
+{
+
+
+
+    //Task 1 - Personalized Welcome Function
+    public static void PrintWelcome(string name)
+    {
+        Console.WriteLine("welcome" + name);
+    }
+    
+    //Task 2 - Square Number Function
+    public static int Square(int number)
+    {
+        return number * number;
+    }
+    
+    
+    
+    
+    //Main 
+    static void Main(string[] args)
+    {
+        
+        //Task 1 - Personalized Welcome Function
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        PrintWelcome(name);
+        
+        
+        //Task 2 - Square Number Function
+        int num = int.Parse(Console.ReadLine());
+        int result = Square(num);
+        Console.WriteLine(result);
+        
+        
+        
+    }
+}
