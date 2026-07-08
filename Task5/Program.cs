a5a67333181d15879d80dc0b8f2ecc33a69fc071
Noor <noor-al-abri@hotmail.com>
2026-07-08 12:34:54 +0400

    Task 1 - Fixed Grades Array

---
+++ Task5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        //Task 1 - Fixed Grades Array
+        int[] grades = new int[5];
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.Write("Enter grade: ");
+            grades[i] = int.Parse(Console.ReadLine());
+        }
+        foreach (int grade in grades)
+        {
+            Console.WriteLine(grade);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
