f4e5544d0153d7da013bdb0f48457bbadcdabb32
Noor <noor-al-abri@hotmail.com>
2026-07-25 21:39:22 +0400

    Add movie insert

--- MovieCollectionEFCore/Program.cs
+++ MovieCollectionEFCore/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace MovieCollectionEFCore;
+﻿using MovieCollectionEFCore.Data;
+using MovieCollectionEFCore.Models;
+
+namespace MovieCollectionEFCore;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Create connection with database
+        using var context = new AppDbContext();
+
+        // DDL: Create Database and Tables
+        context.Database.EnsureCreated();
+        
+        Movie movie = new Movie
+        {
+            Title = "Inception",
+            ReleaseYear = 2010,
+            Rating = 8.8
+        };
+
+        context.Movies.Add(movie);
+        context.SaveChanges();
+        
+        
+        
     }
 }
