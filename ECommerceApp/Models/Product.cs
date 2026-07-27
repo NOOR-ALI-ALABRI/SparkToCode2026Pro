9c79c03ac09cb7123b6cf8b72e8e78f6b493399d
Noor <noor-al-abri@hotmail.com>
2026-07-27 18:53:16 +0400

    Add Category-Product relationship

--- ECommerceApp/Models/Product.cs
+++ ECommerceApp/Models/Product.cs
@@ -5,4 +5,13 @@ public class Product
     public int ProductId { get; set; }
     public string ProductName { get; set; }
     public decimal ProductPrice { get; set; }
+    
+    
+    // Foreign Key
+    public int CategoryId { get; set; }
+    
+    
+    // One Product belongs to one Category (N:1)
+    public Category Category { get; set; }
+    
 }
