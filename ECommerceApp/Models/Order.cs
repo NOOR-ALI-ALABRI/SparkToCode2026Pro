77bf215c5e80e004bf44bd678116d70efbd964e1
Noor <noor-al-abri@hotmail.com>
2026-07-27 17:19:02 +0400

    Add Category entity model

---
+++ ECommerceApp/Models/Order.cs
@@ -0,0 +1,7 @@
+namespace ECommerceApp.Models;
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+}
---
+++ ECommerceApp/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace ECommerceApp.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal ProductPrice { get; set; }
+}
