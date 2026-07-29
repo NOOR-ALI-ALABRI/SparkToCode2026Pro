7c1e3c91fe247ee1e92a691b89abec0f4fd91dc0
Noor <noor-al-abri@hotmail.com>
2026-07-29 22:04:08 +0400

    Updated Program.cs with database context configuration.

--- StudentManagementAPI/Program.cs
+++ StudentManagementAPI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<UniversityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
