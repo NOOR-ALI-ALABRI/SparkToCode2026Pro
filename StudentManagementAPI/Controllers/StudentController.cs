9d1ddc9f1fa98fbd351675cb3bdc13a4b69820f3
Noor <noor-al-abri@hotmail.com>
2026-07-30 11:54:53 +0400

    Add a new student to the database

--- StudentManagementAPI/Controllers/StudentController.cs
+++ StudentManagementAPI/Controllers/StudentController.cs
@@ -1,16 +1,27 @@
 namespace WebApplication1.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 [ApiController]
 [Route ("")]
 public class StudentController : ControllerBase
 {
     //to access the database
-    private UniversityContext _context;
-    public StudentController(UniversityContext context)
+    private UniversityContext context;
+    public StudentController(UniversityContext _context)
     {
-        _context = context;
+        context = _context;
     }
+    
+    [HttpPost]
+    public IActionResult AddStudent(Student s)
+    {
+        context.Students.Add(s);
+        context.SaveChanges();
+        return Ok(s);
+    }
+    
+    
 
 
     
